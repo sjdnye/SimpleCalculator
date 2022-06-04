371971c2354f12c9f40b9ba3b6e3873144a5c3a9
sjdnye <sjdnye@gmail.com>
2022-06-04 20:36:14 +0430

    create viewMdel

--- app/src/main/java/com/example/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -3,16 +3,20 @@ package com.example.calculatorapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.calculatorapp.ui.theme.CalculatorAppTheme
+import com.example.calculatorapp.ui.theme.MediumGray
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,6 +31,15 @@ class MainActivity : ComponentActivity() {
                     val viewModel = viewModel<CalculatorViewModel>()
                     val state = viewModel.state
                     val buttonSpacing = 8.dp
+                    Calculator(
+                        state = state,
+                        onAction = viewModel::onAction,
+                        buttonSpacing = buttonSpacing,
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(MediumGray)
+                            .padding(15.dp)
+                    )
 
 
                 }
